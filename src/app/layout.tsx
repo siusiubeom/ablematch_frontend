3aa9b1ad8c464bd56dbafde48956a67e01a017fb
DONGMIN CHAE <tonkin@hanyang.ac.kr>
2026-02-12 11:44:46 +0900

    description changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
     title: "AbleMatch",
-    description: "AbleMatch",
+    description: "Able Match",
 };
 
 export default function RootLayout({
